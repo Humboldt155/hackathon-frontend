2eb2edf9e3c1273d126e6a0fc870e04eb0b1acfd
Humboldt155 <petr_kozyrev@icloud.com>
2018-09-22 20:19:15 +0300

    4

--- src/views/About.vue
+++ src/views/About.vue
@@ -28,12 +28,18 @@
       participants: [
         {
           id: 'user1',
-          name: 'Matteo',
+          name: 'Veronika',
           imageUrl: 'https://avatars3.githubusercontent.com/u/1915989?s=230&v=4'
         }
       ], // the list of all the participant of the conversation. `name` is the user name, `id` is used to establish the author of a message, `imageUrl` is supposed to be the user avatar.
-      titleImageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png',
+      titleImageUrl: '../../assets/logo.png',
       messageList: [
+          { type: 'text', author: `me`, data: { text: `Say yes!` } },
+          { type: 'text', author: `user1`, data: { text: `No.` } },
+          { type: 'text', author: `me`, data: { text: `Say yes!` } },
+          { type: 'text', author: `user1`, data: { text: `No.` } },
+          { type: 'text', author: `me`, data: { text: `Say yes!` } },
+          { type: 'text', author: `user1`, data: { text: `No.` } },
           { type: 'text', author: `me`, data: { text: `Say yes!` } },
           { type: 'text', author: `user1`, data: { text: `No.` } }
       ], // the list of the messages to show, can be paginated and adjusted dynamically
@@ -42,25 +48,25 @@
       showTypingIndicator: '', // when set to a value matching the participant.id it shows the typing indicator for the specific user
       colors: {
         header: {
-          bg: '#4e8cff',
+          bg: '#5fbd5f',
           text: '#ffffff'
         },
         launcher: {
-          bg: '#4e8cff'
+          bg: '#5fbd5f'
         },
         messageList: {
           bg: '#ffffff'
         },
         sentMessage: {
-          bg: '#4e8cff',
-          text: '#ffffff'
-        },
-        receivedMessage: {
           bg: '#eaeaea',
           text: '#222222'
         },
+        receivedMessage: {
+          bg: '#5FBD5F',
+          text: '#ffffff'
+        },
         userInput: {
-          bg: '#f4f7f9',
+          bg: '#eff2f4',
           text: '#565867'
         }
       }, // specifies the color scheme for the component
